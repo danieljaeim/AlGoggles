39bffcf26af09cd8bb3a16123b2a27afecb57ad0
Daniel Jae Im <45407194+danieljaeim@users.noreply.github.com>
2023-11-06 01:52:21 -0800

    Updates

---
+++ precache-manifest.e41513db30c7882663041e5e4f6565a4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c8e2c144229604023ffca4421b17a1",
+    "url": "/AlGoggles/index.html"
+  },
+  {
+    "revision": "1de88603ff4cc9191d0b",
+    "url": "/AlGoggles/static/css/2.c4a5f2c3.chunk.css"
+  },
+  {
+    "revision": "5536814e2b491b7ca254",
+    "url": "/AlGoggles/static/css/main.9044dc91.chunk.css"
+  },
+  {
+    "revision": "1de88603ff4cc9191d0b",
+    "url": "/AlGoggles/static/js/2.2657671a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/AlGoggles/static/js/2.2657671a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5536814e2b491b7ca254",
+    "url": "/AlGoggles/static/js/main.a9c6dcf3.chunk.js"
+  },
+  {
+    "revision": "ceaed2538a2c5853c7a4",
+    "url": "/AlGoggles/static/js/runtime-main.be274af0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/AlGoggles/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/AlGoggles/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/AlGoggles/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/AlGoggles/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/AlGoggles/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/AlGoggles/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/AlGoggles/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/AlGoggles/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/AlGoggles/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/AlGoggles/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/AlGoggles/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/AlGoggles/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/AlGoggles/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/AlGoggles/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/AlGoggles/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/AlGoggles/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
